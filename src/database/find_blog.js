50b9418cfd725ca22f398644e3d3dc52d289cbc5
Neboer <rubinposter@gmail.com>
2020-09-14 12:46:53 +0800

    逆序展示博文。

--- src/database/find_blog.js
+++ src/database/find_blog.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 
 function list_blogs_by_range(collection, start, length, show_invisible) {
     let find_object = show_invisible ? {} : {visible: true}
-    return collection.find(find_object, {limit: length, skip: start}).toArray() // 不需要进行错误检验，因为没必要检验。
+    return collection.find(find_object).sort({_id:-1}).skip(start).limit(length).toArray() // 不需要进行错误检验，因为没必要检验。
 }
 
 function get_blog_by_id(collection, target_blog_hex_id) {
